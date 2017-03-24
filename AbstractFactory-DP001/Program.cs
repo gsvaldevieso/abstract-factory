2e20a504f652e445d4f2b16e54f1ca8b1c4ce696
Guilherme Valdevieso <gsvaldevieso@gmail.com>
2017-03-23 21:20:56 -0300

    Abstract Factory

---
+++ AbstractFactory-DP001/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AbstractFactoryDP001
+{
+	class MainClass
+	{
+		public static Personagem CriarPersonagem()
+		{
+			PersonagemFactory criadorPersonagem = new PersonagemOgroFactory ();
+			Personagem personagem = criadorPersonagem.ConstruirPersonagem ();
+			Equipamento equipamentoPersonagem = criadorPersonagem.ConstruirEquipamento ();
+			personagem.EquipamentoPrincipal = equipamentoPersonagem;
+
+			return personagem;
+		}
+
+		public static void Main (string[] args)
+		{
+			Personagem personagem = CriarPersonagem ();
+
+			Console.WriteLine ("Você construiu um " + personagem.Descricao + " com o equipamento " + personagem.EquipamentoPrincipal.Descricao + " em mãos");
+			Console.ReadKey ();
+		}
+	}
+}
